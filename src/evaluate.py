3988448bfa476542cd511229c268ac5add3adc7c
Neeraj Negi <neerajnegi108333@gmail.com>
2026-02-21 22:04:59 +0530

    fix the issue

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,11 +1,11 @@
- import numpy as np
- import pandas as pd
- from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
- from sklearn.ensemble import RandomForestRegressor
- import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
  
  
- def evaluate_models(lr_model, dt_model, rf_model, X_test: pd.DataFrame, y_test: pd.Series):
+def evaluate_models(lr_model, dt_model, rf_model, X_test: pd.DataFrame, y_test: pd.Series):
      preds = {
          "LinearRegression": lr_model.predict(X_test),
          "DecisionTreeRegressor": dt_model.predict(X_test),
@@ -24,7 +24,7 @@
      return results
  
  
- def select_best_by_r2(lr_model, dt_model, rf_model, X_test: pd.DataFrame, y_test: pd.Series):
+def select_best_by_r2(lr_model, dt_model, rf_model, X_test: pd.DataFrame, y_test: pd.Series):
      r2_scores = {
          "LinearRegression": r2_score(y_test, lr_model.predict(X_test)),
          "DecisionTreeRegressor": r2_score(y_test, dt_model.predict(X_test)),
@@ -39,7 +39,7 @@
      return best_name, models[best_name]
  
  
- def plot_rf_feature_importance(rf_model: RandomForestRegressor, feature_names, top_n: int = 20):
+def plot_rf_feature_importance(rf_model: RandomForestRegressor, feature_names, top_n: int = 20):
      importances = rf_model.feature_importances_
      idx = np.argsort(importances)[::-1][:top_n]
      names = [feature_names[i] for i in idx]
